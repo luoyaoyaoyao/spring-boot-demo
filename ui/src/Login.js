29ea1d9ff9faf345334b1914086329a439eeccd0
luoyaoyaoyao <1565868733@qq.com>
2019-10-13 18:15:28 +0800

    init project skeleton

---
+++ ui/src/Login.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// import logo from './logo.svg';
+import './App.css';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+
+  }
+
+  render() {
+    const loginUrl = 'http://localhost:8080/login/github?auth_url=' + window.location.href;
+
+    return (
+      <div>
+        
+        <a href={loginUrl}>Github Login</a>
+      </div>
+    )
+  };
+}
+
+
+
+export default Login;
