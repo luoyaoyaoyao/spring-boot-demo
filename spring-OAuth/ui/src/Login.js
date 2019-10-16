a2381e7a4dac60512c3c267a7d6d2c2e6b08cd15
luoyaoyaoyao <1565868733@qq.com>
2019-10-16 22:01:23 +0800

    refactor spring-oAuth

--- spring-OAuth/ui/src/Login.js
+++ spring-OAuth/ui/src/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 // import logo from './logo.svg';
-import './App.css';
 
 class Login extends React.Component {
   constructor(props) {
--- spring-OAuth/ui/src/index.js
+++ spring-OAuth/ui/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Login from './Login';
 import * as serviceWorker from './serviceWorker';
 
