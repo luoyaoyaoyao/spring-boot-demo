d3dc33c378fc9d955f9c27559bb4f6411218f92e
sakura-hlt <1612584677@qq.com>
2019-10-17 20:08:56 +0800

    use try-with-resource to auto-release es client

--- mysql-to-es/src/main/java/com/ly/IndexDoc.java
+++ mysql-to-es/src/main/java/com/ly/IndexDoc.java
@@ -17,53 +17,54 @@ import java.io.IOException;
 public class IndexDoc {
 
     public static void main(String[] args) throws IOException {
-        RestHighLevelClient client = ESClient.getRestHighLevelClient();
+        try (RestHighLevelClient client = ESClient.getRestHighLevelClient()) {
 
-        // 1、创建索引请求
-        IndexRequest request = new IndexRequest(
-                "mess",   //索引
-                "_doc",     // mapping type
-                "1");     //文档id
+            // 1、创建索引请求
+            IndexRequest request = new IndexRequest(
+                    "mess",   //索引
+                    "_doc",     // mapping type
+                    "1");     //文档id
 
-        // 2、准备文档数据
-        request.source(MockData.getData());
+            // 2、准备文档数据
+            request.source(MockData.getData());
 
-        //4、发送请求
-        IndexResponse indexResponse = null;
-        try {
-            // 同步方式
-            indexResponse = client.index(request, RequestOptions.DEFAULT);
-        } catch(ElasticsearchException e) {
-            // 捕获，并处理异常
-            //判断是否版本冲突、create但文档已存在冲突
-            if (e.status() == RestStatus.CONFLICT) {
-                System.out.println("冲突了，请在此写冲突处理逻辑！\n" + e.getDetailedMessage());
+            //4、发送请求
+            IndexResponse indexResponse = null;
+            try {
+                // 同步方式
+                indexResponse = client.index(request, RequestOptions.DEFAULT);
+            } catch (ElasticsearchException e) {
+                // 捕获，并处理异常
+                //判断是否版本冲突、create但文档已存在冲突
+                if (e.status() == RestStatus.CONFLICT) {
+                    System.out.println("冲突了，请在此写冲突处理逻辑！\n" + e.getDetailedMessage());
+                }
+
+                System.out.println("索引异常: " + e.getDetailedMessage());
             }
 
-            System.out.println("索引异常: " + e.getDetailedMessage());
-        }
+            //5、处理响应
+            if (indexResponse != null) {
+                String index = indexResponse.getIndex();
+                String type = indexResponse.getType();
+                String id = indexResponse.getId();
+                long version = indexResponse.getVersion();
+                if (indexResponse.getResult() == DocWriteResponse.Result.CREATED) {
+                    System.out.println("新增文档成功，处理逻辑代码写到这里。");
+                } else if (indexResponse.getResult() == DocWriteResponse.Result.UPDATED) {
+                    System.out.println("修改文档成功，处理逻辑代码写到这里。");
+                }
+                // 分片处理信息
+                ReplicationResponse.ShardInfo shardInfo = indexResponse.getShardInfo();
+                if (shardInfo.getTotal() != shardInfo.getSuccessful()) {
 
-        //5、处理响应
-        if(indexResponse != null) {
-            String index = indexResponse.getIndex();
-            String type = indexResponse.getType();
-            String id = indexResponse.getId();
-            long version = indexResponse.getVersion();
-            if (indexResponse.getResult() == DocWriteResponse.Result.CREATED) {
-                System.out.println("新增文档成功，处理逻辑代码写到这里。");
-            } else if (indexResponse.getResult() == DocWriteResponse.Result.UPDATED) {
-                System.out.println("修改文档成功，处理逻辑代码写到这里。");
-            }
-            // 分片处理信息
-            ReplicationResponse.ShardInfo shardInfo = indexResponse.getShardInfo();
-            if (shardInfo.getTotal() != shardInfo.getSuccessful()) {
-
-            }
-            // 如果有分片副本失败，可以获得失败原因信息
-            if (shardInfo.getFailed() > 0) {
-                for (ReplicationResponse.ShardInfo.Failure failure : shardInfo.getFailures()) {
-                    String reason = failure.reason();
-                    System.out.println("副本失败原因：" + reason);
+                }
+                // 如果有分片副本失败，可以获得失败原因信息
+                if (shardInfo.getFailed() > 0) {
+                    for (ReplicationResponse.ShardInfo.Failure failure : shardInfo.getFailures()) {
+                        String reason = failure.reason();
+                        System.out.println("副本失败原因：" + reason);
+                    }
                 }
             }
         }
--- mysql-to-es/src/main/java/com/ly/InitIndex.java
+++ mysql-to-es/src/main/java/com/ly/InitIndex.java
@@ -10,46 +10,46 @@ import java.io.IOException;
 
 public class InitIndex {
     public static void main(String[] args) throws IOException {
-        RestHighLevelClient client = ESClient.getRestHighLevelClient();
+        try (RestHighLevelClient client = ESClient.getRestHighLevelClient()) {
 
-        // 1、创建 创建索引request 参数：索引名mess
-        CreateIndexRequest indexRequest = new CreateIndexRequest("mess");
+            // 1、创建 创建索引request 参数：索引名mess
+            CreateIndexRequest indexRequest = new CreateIndexRequest("mess");
 
-        // 2、设置索引的settings
+            // 2、设置索引的settings
 //        indexRequest.settings(Settings.builder().put("index.number_of_shards", 3) // 分片数
 //                .put("index.number_of_replicas", 2) // 副本数
 //                .put("analysis.analyzer.default.tokenizer", "ik_smart") // 默认分词器
 //        );
 
-        // 3、设置索引的mappings
-        indexRequest.mapping("_doc",
-                "  {\n" +
-                        "    \"_doc\": {\n" +
-                        "      \"properties\": {\n" +
-                        "        \"message\": {\n" +
-                        "          \"type\": \"text\"\n" +
-                        "        }\n" +
-                        "      }\n" +
-                        "    }\n" +
-                        "  }",
-                XContentType.JSON);
+            // 3、设置索引的mappings
+            indexRequest.mapping("_doc",
+                    "  {\n" +
+                            "    \"_doc\": {\n" +
+                            "      \"properties\": {\n" +
+                            "        \"message\": {\n" +
+                            "          \"type\": \"text\"\n" +
+                            "        }\n" +
+                            "      }\n" +
+                            "    }\n" +
+                            "  }",
+                    XContentType.JSON);
 
-        // 4、 设置索引的别名
+            // 4、 设置索引的别名
 //        indexRequest.alias(new Alias("mmm"));
 
-        // 5、 发送请求
-        // 5.1 同步方式发送请求
-        CreateIndexResponse createIndexResponse = client.indices()
-                .create(indexRequest, RequestOptions.DEFAULT);
+            // 5、 发送请求
+            // 5.1 同步方式发送请求
+            CreateIndexResponse createIndexResponse = client.indices()
+                    .create(indexRequest, RequestOptions.DEFAULT);
 
-        // 6、处理响应
-        boolean acknowledged = createIndexResponse.isAcknowledged();
-        boolean shardsAcknowledged = createIndexResponse
-                .isShardsAcknowledged();
-        System.out.println("acknowledged = " + acknowledged);
-        System.out.println("shardsAcknowledged = " + shardsAcknowledged);
+            // 6、处理响应
+            boolean acknowledged = createIndexResponse.isAcknowledged();
+            boolean shardsAcknowledged = createIndexResponse
+                    .isShardsAcknowledged();
+            System.out.println("acknowledged = " + acknowledged);
+            System.out.println("shardsAcknowledged = " + shardsAcknowledged);
 
-        // 5.1 异步方式发送请求
+            // 5.1 异步方式发送请求
             /*ActionListener<CreateIndexResponse> listener = new ActionListener<CreateIndexResponse>() {
                 @Override
                 public void onResponse(
@@ -71,5 +71,6 @@ public class InitIndex {
 
             client.indices().createAsync(indexRequest, listener);
             */
+        }
     }
 }
